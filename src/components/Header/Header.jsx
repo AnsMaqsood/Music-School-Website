30c11b5310df1bddd27a57197f3a241260e55322
Ans Maqsood <anschaudhary55@gmail.com>
2024-08-19 09:29:08 +0500

    Added animation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
 import "./Header.css"
 import Navbar from "../Navbar/Navbar.jsx"
+import { useState, useEffect } from 'react'
 
 
 const Header = () => {
+
+    const [animate, setAnimate] = useState(false);
+  
+    useEffect(() => {
+      setAnimate(true);
+    }, []);
+
   return (
     <header>
       <div className='wrapper'>
-        <Navbar/>
+        <Navbar />
         <div className='cta'>
-      <p className='course-name'>Sound Design Masterclass</p>
-      <h1>Learn the Art of Sound Design</h1>
-      <a href="#" className='demo-btn'>Demo Lesson</a>
-      </div>
+          <p className='course-name'>Sound Design Masterclass</p>
+          <h1>Learn the Art of Sound Design</h1>
+          <a href="#" className='demo-btn'>Demo Lesson</a>
+        </div>
       </div>
     </header>
   )
